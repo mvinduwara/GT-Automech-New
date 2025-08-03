b4cb00462bdcf334fbf7dcf9f3f8a987bd44a070
JanithNimhara <137036759+janithnm@users.noreply.github.com>
2025-08-03 21:53:14 +0530

    user-seeder

--- gt-erp/database/seeders/UserSeeder.php
+++ gt-erp/database/seeders/UserSeeder.php
@@ -12,6 +12,34 @@ class UserSeeder extends Seeder
 
     public function run(): void
     {
+
+        User::create([
+            'name' => 'C.D Kuruppu',
+            'email' => 'cdkuruppu78@gmail.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'mobile' => '0774098580',
+            'role' => 'admin',
+            'status' => 'active',
+        ]);
+        User::create([
+            'name' => 'Kumudu Kariyapperuma',
+            'email' => 'kumuisha7@gmail.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'mobile' => '0742120450',
+            'role' => 'service-manager',
+            'status' => 'active',
+        ]);
+        User::create([
+            'name' => 'Pulinda Nanayakkara',
+            'email' => 'pulindananayakkara14874@gmail.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'mobile' => '0711386286',
+            'role' => 'admin',
+            'status' => 'active',
+        ]);
         User::create([
             'name' => 'Shehan Kaushalya',
             'email' => 'shehankmusic@gmail.com',
